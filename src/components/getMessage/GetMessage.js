fa24459cc577e3f24b4f1d518c4a71d15e05b985
Mustafa <diertojiev@yandex.ru>
2025-01-31 15:57:41 +0300

    Send message from input site

--- src/components/getMessage/GetMessage.js
+++ src/components/getMessage/GetMessage.js
@@ -1,4 +1,3 @@
-// components/GetMessage.jsx
 import { useEffect, useState } from "react";
 import { useHttp } from "../../hooks/http.hook";
 
@@ -19,29 +18,43 @@ const GetMessage = ({ activeChat }) => {
 
 				console.log(response);
 
-				if (
-					(response?.body &&
-						response?.body.typeWebhook === "incomingMessageReceived") ||
-					response?.body.typeWebhook === "outgoingMessageReceived"
-				) {
-					const newMessage = {
-						text: response.body.messageData.textMessageData.textMessage,
-						from: response.body.senderData.chatId.replace("@c.us", ""),
-						timestamp: new Date().toLocaleTimeString(),
-					};
+				if (response?.body) {
+					const { typeWebhook, messageData, senderData } = response.body;
 
-					setMessages((prev) => [...prev, newMessage]);
+					if (
+						typeWebhook === "incomingMessageReceived" ||
+						typeWebhook === "incomingAPIReceived" ||
+						typeWebhook === "outgoingMessageReceived" ||
+						typeWebhook === "outgoingAPIMessageReceived"
+					) {
+						let text = "";
 
-					// Удаляем уведомление после обработки
-					await request(
-						`https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${response.receiptId}`,
-						"DELETE"
-					);
-				} else {
-					await request(
-						`https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${response.receiptId}`,
-						"DELETE"
-					);
+						if (messageData?.textMessageData?.textMessage) {
+							text = messageData.textMessageData.textMessage;
+						} else if (messageData?.extendedTextMessageData?.text) {
+							text = messageData.extendedTextMessageData.text;
+						}
+
+						if (text) {
+							const newMessage = {
+								text: text,
+								from: senderData.chatId.replace("@c.us", ""),
+								timestamp: new Date().toLocaleTimeString(),
+							};
+
+							setMessages((prev) => [...prev, newMessage]);
+						}
+						// Удаляем уведомление после обработки
+						await request(
+							`https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${response.receiptId}`,
+							"DELETE"
+						);
+					} else {
+						await request(
+							`https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${response.receiptId}`,
+							"DELETE"
+						);
+					}
 				}
 			} catch (e) {
 				console.error("Ошибка получения сообщений:", e);
